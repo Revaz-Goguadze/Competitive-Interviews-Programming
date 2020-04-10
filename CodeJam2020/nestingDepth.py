374f5bc5ba69e94cf0bca8692bf55744d0146e40
Jorge <sandix390@gmail.com>
2020-04-10 12:32:40 -0500

    Upadting

--- CodeJam2020/nestingDepth.py
+++ CodeJam2020/nestingDepth.py
@@ -4,27 +4,33 @@ def solve():
     res = ""
     n = input()
     n = str(n)
+
     if(len(n) == 1):
         res+="("* int(n)
         res+=n
         res+=")" *int(n)
         return res
+
     opens = 0
     for index in range(len(n)):
         current = int(n[index])
         if(current == opens):
             res+=str(current)
+            
         elif(current > opens):
             res+="("*(current-opens)
             res+=str(current)
             opens+=current-opens
+
         elif(current<opens):
             res+=")"*abs(opens-current)
             res+=str(current)
             opens-=abs(opens-current)
+
     for i in range(opens):
         res+=")"
     return res
+
 
 # Input
 t = int(input())
